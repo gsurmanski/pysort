83c6d3d9111c5b2cb6241fa923747d0114cfa5fc
Geoff Surmanski <gsurmans@gmail.com>
2024-08-04 14:02:23 -0400

    Update mysort.py

--- mysort.py
+++ mysort.py
@@ -1,14 +1,14 @@
-unsorted_list = [1,2,3]
-
+unsorted_list = [5, 8, 3, -100, 100, 16, 4, 2, 1]
+# This function sorts the list by iterating through each element and swapping it with any smaller element found ahead in the nested loop.
+# It repeats this process for each element in the outer loop, effectively pushing the smallest elements to the front of the list.
 def mysort(unsorted_list):
     length = len(unsorted_list)
     for i in range(length):
         for j in range(i + 1, length):
-            print(i,j)
             if unsorted_list[i] > unsorted_list[j]:
                 unsorted_list[i], unsorted_list[j] = unsorted_list[j], unsorted_list[i]
     
-    #return unsorted_list
+    return unsorted_list
 
 print(mysort(unsorted_list))
         
